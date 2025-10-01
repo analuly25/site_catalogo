ce1ac0d52dd026bc50c1e4b5f91b96c61744fe67
analuly25 <analuizagsantana@gmail.com>
2025-09-30 21:31:43 -0300

    ajustes

--- script.js
+++ script.js
@@ -3,5 +3,14 @@ function showDetails(id) {
   document.querySelectorAll(".detail").forEach(det => det.classList.add("hidden"));
 
   // Mostra apenas o detalhamento escolhido
-  document.getElementById(id).classList.remove("hidden");
+  const detail = document.getElementById(id);
+  detail.classList.remove("hidden");
+
+  // Faz a página rolar até o detalhe
+  detail.scrollIntoView({ behavior: "smooth", block: "start" });
+
+}
+
+function openYoutube(url) {
+  window.open(url, "_blank"); // abre em nova aba
 }
